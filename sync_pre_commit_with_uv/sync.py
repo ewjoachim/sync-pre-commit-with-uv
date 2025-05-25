4e3cf6c36d3104d2e4082602af65299fb8c9a474
Joachim Jablon <ewjoachim@gmail.com>
2025-05-25 19:42:13 +0200

    fix pre-commit config

--- sync_pre_commit_with_uv/sync.py
+++ sync_pre_commit_with_uv/sync.py
@@ -201,6 +201,7 @@ def uv_export(params: list[str]) -> list[str]:
     base_export_args = [
         "uv",
         "export",
+        "--active",
         "--no-hashes",
         "--no-header",
         "--no-emit-project",
